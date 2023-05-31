e7c69694759ec3c1c1f3f2cb9eb6f03c418ea3f4
fkartal33 <100225125+fkartal33@users.noreply.github.com>
2023-05-31 16:20:48 +0300

    Update BaseTest.java

--- src/test/java/com/testinium/base/BaseTest.java
+++ src/test/java/com/testinium/base/BaseTest.java
@@ -91,7 +91,7 @@ public class BaseTest {
                 capabilities.setCapability(ChromeOptions.CAPABILITY, options);
                 capabilities.setCapability("key", System.getenv("key"));
                 browserName = System.getenv("browser");
-                driver = new RemoteWebDriver(new URL("https://hubclouddev.testinium.com/wd/hub"), capabilities);
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
                 actions = new Actions(driver);
             }
         } catch (MalformedURLException e) {
